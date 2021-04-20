f7d68d56cc0df977798a5855c4f0361fdd64edc0
WickedyWick <sestudent321@gmail.com>
2021-04-20 14:45:47 +0200

    BugFix
    Popravljeno ispisvanje igraca na player listi

--- public/javascripts/mysql.js
+++ public/javascripts/mysql.js
@@ -78,6 +78,7 @@ function joinRoomSQL(socket,room,username,localData){
                                                 'roomCode': room,
                                                 'sessionToken': sessionToken
                                             })
+
                                         }
                                     } )
                                     
